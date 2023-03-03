5e9cf7802577e3492ec52d4a404281028a7f1d64
Nikita-Golubtsov <nproger781@gmail.com>
2023-03-03 19:50:26 +0700

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Main from '../componentsPage/Main/Main';
+// import Vacancies from '../componentsPage/Vacancies/Vacancies';
+// import Vacancy from '../componentsPage/Vacancy/Vacancy';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentsPage/Main/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import Menu from '../../componentsBlocks/Menu/Menu';
+
+import '../Main/Main.scss';
+
+function Main(params) {
+    return (
+        <div className="wrapper">
+
+        </div>
+    );
+}
+
+export default Main;
