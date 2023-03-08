8686337729fc77b9f13d2ad72706b480f68ff442
Nikita-Golubtsov <nproger781@gmail.com>
2023-03-08 11:35:16 +0700

    style footer

--- src/componentsBlocks/Footer/Footer.js
+++ src/componentsBlocks/Footer/Footer.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import './Footer.scss';
 
 function Footer() {
     return (
         <footer>
-
+            <div className="container">
+                <nav>
+                    <ul>
+                        <li><Link to="/" className="footer-link">Главная</Link></li>
+                        <li><Link to="/vacancies" className="footer-link">Вакансии</Link></li>
+                        <li><Link to="/works" className="footer-link">Направления</Link></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="copy">
+                <p className="text-copy">© 2023 Work Developer</p>
+            </div>
         </footer>
     )
 }
