9b6f9ad693c0bebf13df8129a96ad6ac79e4bd35
Nikita-Golubtsov <nproger781@gmail.com>
2023-03-14 16:27:53 +0700

    description professions

--- src/componentsPage/Work/Work.js
+++ src/componentsPage/Work/Work.js
@@ -1,56 +1,74 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
 import Footer from "../../componentsBlocks/Footer/Footer";
 
 import './Work.scss';
 
+const listWorks = require('./list-works/list-works.json');
+
 function Work() {
+
+    const refs = [];
+
+    listWorks.map(el => {
+        let itemRef = React.createRef();
+        refs.push(itemRef);
+    });
+
+    useEffect(() => { }, [listWorks]);
+
+    const checkActive = (event, el) => {
+        if (event.target.tagName === 'ION-ICON') {
+            let elRefs = refs[event.target.id].current;
+            // if (elRefs.classList.contains('active')) {
+            // closeInfoProfessions();
+            // } else {
+            // setTimeout(() => getInfoProfessions(elRefs), 500);
+            // }
+            getInfoProfessions(elRefs)
+        }
+    }
+
+    const getInfoProfessions = (el) => {
+        closeInfoProfessions();
+        el.classList.add('active');
+        // el.style.display = 'block';
+        el.style.height = el.scrollHeight + 'px';
+        el.style.opacity = 1;
+    }
+
+    const closeInfoProfessions = () => {
+        refs.map(el => {
+            el.current.classList.remove('active');
+            el.current.style.opacity = 0;
+            el.current.style.height = 0;
+            setTimeout(() => {
+                // el.current.style.display = 'none';
+            }, 400);
+        });
+    }
+
+
     return (
         <>
             <div className="blc-work">
                 <div className="container">
-                    <div id="web-developer" className="elem-work">
-                        <div className="item-name">
-                            <p className="name">Web-разработка</p>
-                            <span className="icon"><ion-icon name="arrow-down-outline"></ion-icon></span>
-                        </div>
-                        <div className="item-text">
-                            <ul className="list-prof">
-                                <li className="link-prof">Frontend-разработчик</li>
-                                <li className="link-prof">Backend-разработчик</li>
-                                <li className="link-prof">Мобильный разработчик</li>
-                                <li className="link-prof">Fullstack-разработчик</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div id="web-developer" className="elem-work">
-                        <div className="item-name">
-                            <p className="name">Web-разработка</p>
-                            <span className="icon"><ion-icon name="arrow-down-outline"></ion-icon></span>
-                        </div>
-                        <div className="item-text">
-                            <ul className="list-prof">
-                                <li className="link-prof">Frontend-разработчик</li>
-                                <li className="link-prof">Backend-разработчик</li>
-                                <li className="link-prof">Мобильный разработчик</li>
-                                <li className="link-prof">Fullstack-разработчик</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div id="web-developer" className="elem-work">
-                        <div className="item-name">
-                            <p className="name">Web-разработка</p>
-                            <span className="icon"><ion-icon name="arrow-down-outline"></ion-icon></span>
-                        </div>
-                        <div className="item-text">
-                            <ul className="list-prof">
-                                <li className="link-prof">Frontend-разработчик</li>
-                                <li className="link-prof">Backend-разработчик</li>
-                                <li className="link-prof">Мобильный разработчик</li>
-                                <li className="link-prof">Fullstack-разработчик</li>
-                            </ul>
-                        </div>
-                    </div>
+                    {
+                        listWorks.map((el, index) => (
+                            <div id={`${el.id}`} key={index} className="elem-work">
+                                <div className="item-name">
+                                    <p className="name">{el.name}</p>
+                                    <span className="icon"><ion-icon onClick={checkActive} id={`${index}`} name="arrow-down-outline"></ion-icon></span>
+                                </div>
+                                <div className="item-text" ref={refs[index]}>
+                                    <ul className="list-prof">
+                                        {el.works.map((el, index) => (
+                                            <li key={index} className="link-prof">{el[0]} - {el[1]}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
             <Footer />
